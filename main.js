7958f8ded538e1a03737ea241d49a9fed1927b6f
Kristina Naylor <kristinamarienaylor@gmail.com>
2020-08-08 15:10:46 -0400

    Add directions to each layer

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
        setTimeout(function() {
            $('#colors').hide();
            $('#numbers').show();
+           $('#directions-p').html("Now, pick a number.")
         }, $(this).text().length * 2000);
        });
 //Any Numbers
@@ -22,6 +23,7 @@ $(document).ready(function() {
         setTimeout(function() {
             $('#numbers').hide();
             $('#numbers-last').show();
+            $('#directions-p').html("Pick one more number.")
         }, parseInt($(this).text()) * 2000);
      });
 //Any Last Numbers
@@ -35,8 +37,16 @@ $(document).ready(function() {
         setTimeout(function() {
             $('#numbers-last').hide();
             $('#message').show();
+            $('#directions-p').html("Your fortune has been revealed!")
         }, parseInt($(this).text()) * 2000);
      });
     });
+//Random Fortune
+    var strings = ["Someone has Googled you recently.","You are good enough, smart enough, and goshdarn it, people like you!", "Plan for an adventure this weekend.", "Don't be afraid to take that big step.","Don't pursue happiness, create it!", "A ship in harbor is safe, but that's not why ships are built."];  // Store the possible strings in an array
 
- 
+    var btn = document.getElementById('numbers-last');
+    var output = document.getElementById('messagep');
+
+    btn.addEventListener("click", function(){
+    output.innerHTML = strings[Math.floor(Math.random() * strings.length)];
+    }); 

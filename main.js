55c1c1674b31cd8afc963ef217b014b720468b5a
Kristina Naylor <kristinamarienaylor@gmail.com>
2020-08-08 20:00:46 -0400

    Add meta tags, index all code uniformly, and combine shard box styles

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
      });
     });
 //Random Fortune
-    var strings = ["Someone has Googled you recently.","You are good enough, smart enough, and goshdarn it, people like you!", "Plan for an adventure this weekend. Don't let Corona stand in your way!", "Don't be afraid to take that big step.","Don't pursue happiness, create it!", "A ship in harbor is safe, but that's not why ships are built."];  // Store the possible strings in an array
+    var strings = ["Someone has Googled you recently.","You are good enough, smart enough, and goshdarn it, people like you!", "Plan for an adventure this weekend. Don't let Corona stand in your way!", "Don't be afraid to take that big step.","Don't pursue happiness, create it!", "A ship in harbor is safe, but that's not why ships are built.", "If you want the rainbow, you have to tolerate some rain.", "He who throws dirt is losing ground.", "Organize your life around your dreams, and watch them come true.", "To avoid criticism, do nothing and say nothing.", "If you can imagine it, you can achieve it.", "If you can dream it, you can become it."];
 
     var btn = document.getElementById('numbers-last');
     var output = document.getElementById('messagep');
